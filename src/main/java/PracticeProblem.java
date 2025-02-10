54d5976f1804a8a428cfc4c5b5452d677f6d2488
jathav-puvirajan-ics4u1-03-25 <jathav.puvirajan@student.tdsb.on.ca>
2025-02-10 20:00:46 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -18,58 +18,124 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		double num = in.nextDouble();
+		in.nextLine();
+		System.out.println(Math.abs(num));
 		
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		double num1 = in.nextDouble();
+		in.nextLine();
+		System.out.print("Input another number: ");
+		double num2 = in.nextDouble();
+		in.nextLine();
+		System.out.println(Math.floor(num1/num2));
+		System.out.println(Math.ceil(num1/num2));
 		
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		double num = in.nextDouble();
+		in.nextLine();
+		System.out.println(Math.round(Math.sqrt(num)));
 		
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		double num1 = in.nextDouble();
+		in.nextLine();
+		System.out.print("Input another number: ");
+		double num2 = in.nextDouble();
+		in.nextLine();
+		System.out.println(Math.pow(num1, num2));
 		
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		double num1 = in.nextDouble();
+		in.nextLine();
+		System.out.print("Input another number: ");
+		double num2 = in.nextDouble();
+		in.nextLine();
+		System.out.print("Input one more number: ");
+		double num3 = in.nextDouble();
+		in.nextLine();
+		System.out.println(Math.max(Math.max(num1,num2),num3));
+		System.out.println(Math.min(Math.min(num1,num2),num3));
+		
 		
 	}
 	public static void q6() {
-		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a sentence: ");
+		String str = in.nextLine();
+		System.out.println(str.contains("on"));
 		
 	}
 	public static void q7() {
-		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input the word mango: ");
+		String str = in.nextLine();
+		System.out.println(str.equalsIgnoreCase("mango"));
 		
 	}
 	public static void q8() {
-		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a word: ");
+		String str = in.nextLine();
+		System.out.print("Input a letter: ");
+		String chr = in.nextLine();
+		System.out.println(str.indexOf(chr));
+		System.out.println(str.lastIndexOf(chr));
 		
 	}
 
 	public static void q9() {
-		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a sentence: ");
+		String str = in.nextLine();
+		System.out.println("Your sentence is " + str.length() + " characters long");
 		
 	}
 	public static void q10() {
-		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a sentence: ");
+		String str = in.nextLine();
+		System.out.print("Input a word to replace: ");
+		String str2 = in.nextLine();
+		System.out.print("What word would you like to replace it with: ");
+		String str3 = in.nextLine();
+		System.out.println(str.replaceAll(str2,str3));
 		
 	}
 
 	public static void q11() {
-		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a sentence: ");
+		String str = in.nextLine().trim();
+		System.out.println(str.toUpperCase());
+		System.out.println(str.toLowerCase());
 		
 	}
 
 	public static void q12() {
-		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a word: ");
+		String str = in.nextLine();
+		System.out.println(str.substring(0,4));
+		
+		System.out.println(str.substring(str.length()-4,str.length()));
 		
 	}
 	
